409de2719831a4bfb299106ecce06ab0ee0a7ef7
Vikrant Yadav <vikrantmani45@gmail.com>
2024-09-26 19:28:41 +0530

    first commit

---
+++ reunion-table/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DataTable from './components/DataTable'; 
+import './App.css'; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>Advanced Data Table</h1>
+      <DataTable />
+    </div>
+  );
+};
+
+export default App;
